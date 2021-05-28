4ae9e2425725a46800263deb40a6eb9c926c7e15
Elena Benalcazar <elena_benalcazar_rodas@hotmail.com>
2021-05-27 23:55:11 -0300

    agregué funciones faltantes del panel de texto y del botón de descarga

--- app.js
+++ app.js
@@ -113,14 +113,27 @@ const resetFiltros = () => {
   actualizarFiltros();
 };
 botonRestablecer.addEventListener("click", resetFiltros);
-
-//TEXTO - CAMBIAR TEXTOS 
+////////////////////////TEXTO///////////////////////////////
+//CAMBIAR TEXTOS 
 //TEXTO ARRIBA
 const inpTextSup = document.getElementById("inpTextSup");
 const textoArriba = document.getElementById("textoArriba");
 
 inpTextSup.addEventListener("input", () =>{
   textoArriba.innerText = inpTextSup.value;
+});
+
+const sinTextoArriba = document.getElementById("sinTextoArriba");
+sinTextoArriba.addEventListener("click", () =>{
+  if (sinTextoArriba.checked){
+    inpTextSup.disabled = true;
+    textoArriba.textContent = "";
+    textoArriba.classList.add("ocultar");
+  } else {
+    inpTextSup.disabled = false;
+    textoArriba.classList.remove("ocultar");
+    textoArriba.textContent = inpTextSup.value;
+  }
 });
 //TEXTO ABAJO
 const inpTextAba = document.getElementById("inpTexAba");
@@ -129,3 +142,119 @@ const textoAbajo = document.getElementById("textoAbajo");
 inpTextAba.addEventListener("input", () =>{
   textoAbajo.innerText = inpTextAba.value;
 });
+
+const sinTextoAbajo = document.getElementById("sinTextoAbajo");
+sinTextoAbajo.addEventListener("click", () =>{
+  if (sinTextoAbajo.checked){
+    inpTextAba.disabled = true;
+    textoAbajo.textContent = "",
+    textoAbajo.classList.add("ocultar");
+  } else {
+    inpTextAba.disabled = false;
+    textoAbajo.classList.remove("ocultar");
+    textoAbajo.textContent = inpTextAba.value;
+  }
+});
+
+ // FUENTES
+ const fuente = document.getElementById("fuente");
+ fuente.addEventListener("input", () => {
+   textoArriba.style.fontFamily = fuente.value;
+   textoAbajo.style.fontFamily = fuente.value;
+ });
+ //TAMAÑO FUENTE
+ const tamanioFuente = document.getElementById("tamanioFuente");
+ tamanioFuente.addEventListener("input", () => {
+   textoArriba.style.fontSize = `${tamanioFuente.value}px`;
+   textoAbajo.style.fontSize = `${tamanioFuente.value}px`;
+ });
+//ALINEACION
+const alineacionIzq = document.getElementById("alineacionIzq");
+alineacionIzq.addEventListener("click", () =>{
+  textoArriba.style.textAlign = "left";
+  textoAbajo.style.textAlign = "left";
+});
+const alineacionCen = document.getElementById("alineacionCen");
+alineacionCen.addEventListener("click", () =>{
+  textoArriba.style.textAlign = "center";
+  textoAbajo.style.textAlign = "center";
+});
+const alineacionDer = document.getElementById("alineacionDer");
+alineacionDer.addEventListener("click", () =>{
+  textoArriba.style.textAlign = "right";
+  textoAbajo.style.textAlign = "right";
+});
+//COLOR
+const inpColorTxt = document.getElementById("inpColorTxt");
+const spanColorTxt = document.getElementById("spanColorTxt");
+inpColorTxt.addEventListener("change", () =>{
+  textoArriba.style.color = inpColorTxt.value;
+  textoAbajo.style.color = inpColorTxt.value;
+  spanColorTxt.textContent = inpColorTxt.value;
+});
+//FONDO
+const inputFondoTexto = document.getElementById("inputFondoTexto");
+const spanFondoTexto = document.getElementById("spanFondoTexto");
+
+inputFondoTexto.addEventListener("change", () =>{
+  textoArriba.style.backgroundColor = inputFondoTexto.value;
+  textoAbajo.style.backgroundColor = inputFondoTexto.value;
+  spanFondoTexto.textContent = inputFondoTexto.value
+});
+
+const fondoTransparente = document.getElementById("fondoTransparente");
+fondoTransparente.addEventListener("click", () =>{
+  if (fondoTransparente.checked){
+    textoArriba.style.backgroundColor = "transparent";
+    textoAbajo.style.backgroundColor = "transparent";
+    imgMeme.style.backgroundImageSize = "extend";
+  } else {
+    textoArriba.style.backgroundColor = inputFondoTexto.value;
+    textoAbajo.style.backgroundColor = inputFondoTexto.value;
+  }
+});
+
+//CONTORNO TEXTOS
+//sin contorno
+const sinContorno = document.getElementById("sinContorno");
+sinContorno.addEventListener("click", ()=>{
+  textoArriba.classList.remove("contornoTextoClaro");
+  textoAbajo.classList.remove("contornoTextoClaro");
+  textoArriba.classList.remove("contornoTextoOscuro");
+  textoAbajo.classList.remove("contornoTextoOscuro");
+});
+//contorno claro
+const contClaro = document.getElementById("contClaro");
+contClaro.addEventListener("click", () =>{
+  textoArriba.classList.add("contornoTextoClaro");
+  textoAbajo.classList.add("contornoTextoClaro");
+});
+//contorno oscuro
+const contOscuro = document.getElementById("contOscuro");
+contOscuro.addEventListener("click", () =>{
+  textoArriba.classList.add("contornoTextoOscuro");
+  textoAbajo.classList.add("contornoTextoOscuro");
+});
+
+//ESPACIADO
+const espaciado = document.getElementById("espaciado");
+espaciado.addEventListener("input", () =>{
+  textoArriba.style.padding = `${espaciado.value}px`;
+  textoAbajo.style.padding = `${espaciado.value}px`;
+});
+//INTERLINEADO
+const interlineado = document.getElementById("interlineado");
+interlineado.addEventListener("input", ()=>{
+  textoArriba.style.lineHeight = interlineado.value;
+  textoAbajo.style.lineHeight = interlineado.value;
+});
+
+//BOTON DE DESCARGA
+const descargarMeme = document.getElementById("descargarMeme");
+const memeContainer = document.getElementsByClassName("meme-container");
+descargarMeme.addEventListener("click", () =>{
+  domtoimage.toBlob(document.querySelector(".meme-container")).then(function(blob){
+    window.saveAs(blob, "miMeme.png");
+  });
+});
+
